728a24f8968f64a36ff0e2eb22b220944681326b
Ryland <rylanddev@gmail.com>
2018-04-12 22:29:41 -0700

    Start

---
+++ game.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    Silentcrest.display = GenCon({
+        defaultBg: BLACK,
+        defaultFg: WHITE
+    });
+    Silentcrest.display.addTo("displayplace");
+
+    Silentcrest.update = Silentcrest.startMenu();
+
+    window.onkeypress = function (e) {
+        Silentcrest.update(e);
+    };
+};
---
+++ startmenu.js
@@ -0,0 +1,56 @@
+Silentcrest.startMenu = function () {
+    var options = [{
+        text: "Start",
+        callback: function () {
+            //Silentcrest.update = Silentcrest.play();
+        }
+    }, {
+        text: "Credits",
+        callback: function () {}
+    }, {
+        text: "Bleb",
+        callback: function () {}
+    }];
+    var selection = 0;
+
+    Silentcrest.display.clear();
+
+    for (var i = 0; i < options.length; i++) {
+        Silentcrest.display.putString(2, i, options[i].text);
+    };
+
+    Silentcrest.display.set(0, selection, {
+        character: ">",
+    });
+
+    return function (e) {
+        var key = String.fromCharCode(e.keyCode || e.which);
+        if (key === "j") {
+            Silentcrest.display.set(0, selection, {
+                character: " "
+            });
+            selection++;
+            if (selection >= options.length) {
+                selection = 0;
+            }
+            Silentcrest.display.set(0, selection, {
+                character: ">"
+            });
+        }
+        if (key === "k") {
+            Silentcrest.display.set(0, selection, {
+                character: " "
+            });
+            selection--;
+            if (selection < 0) {
+                selection = options.length - 1;
+            }
+            Silentcrest.display.set(0, selection, {
+                character: ">"
+            });
+        }
+        if (key === "x") {
+            options[selection].callback();
+        }
+    };
+};
